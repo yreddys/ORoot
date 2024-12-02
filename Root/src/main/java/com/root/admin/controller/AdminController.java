f264004e072d8909c666d469c65ee03824874105
yreddys <yreddys211@gmail.com>
2024-12-02 13:10:04 +0530

    changes in admin controller

--- Root/src/main/java/com/root/admin/controller/AdminController.java
+++ Root/src/main/java/com/root/admin/controller/AdminController.java
@@ -8,6 +8,7 @@ import javax.sql.rowset.serial.SerialException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +19,7 @@ import com.root.admin.service.AdminService;
 
 @RestController
 @RequestMapping("/admin")
+@CrossOrigin(origins = "*")
 public class AdminController {
 	@Autowired
 	private AdminService adminService;
